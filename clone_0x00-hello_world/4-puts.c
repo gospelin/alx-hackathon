dbc9cb03f457a4c4eda1910c178dba78fb061a9b
gospelin <gospelin.gi@gmail.com>
2022-04-29 21:39:35 +0100

    4-puts.c

---
+++ clone_0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints string using puts function
+ *
+ * return - Always 0
+ *
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
