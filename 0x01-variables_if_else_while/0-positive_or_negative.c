bd47c1b0e4ab72614eef055bae3a7dd40788dc71
gospelin <gospelin.gi@gmail.com>
2022-04-30 22:35:57 +0100

    fix errors and add new files

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints number with respective sign
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+
+	if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	} else if(n > 0)
+	{
+		printf("%d is positive\n", n);
+	} else
+	{
+		printf("%d is zero\n", n);
+	}
+	return (0);
+}
---
+++ clone_0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *	main - prints string using printf function
+ *
+ *	Return: returns 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
