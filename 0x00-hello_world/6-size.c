e570a856d957c789a8bb65a4e6aabfa21778a9f7
gospelin <gospelin.gi@gmail.com>
2022-05-03 20:16:03 +0100

    rename directory

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Determines the various sizes of
+ * the different data types
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	printf("%d", sizeof(int));
+}
