3eb9301c51c8ddcaa92f16518884d1ef45e02cca
gospelin <gospelin.gi@gmail.com>
2022-07-21 00:30:42 +0100

    main.h

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,12 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+int _putchar(char);
+char *create_array(unsigned int size, char c);
+int _strlen(char *s);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+
+#endif
