f0c3bc80226c773be97531799d1a683819a4d94f
gospelin <gospelin.gi@gmail.com>
2022-11-21 01:01:51 +0100

    First Task

--- alx-higher_level_programming/AirBnB_clone/models/base_model.py
+++ alx-higher_level_programming/AirBnB_clone/models/base_model.py
@@ -7,20 +7,35 @@ attributes: id, created_at and updated_at
 methods: save() and to_json()
 """
 
-import uuid
 from datetime import datetime
+import uuid
 
-class BaseModels:
+class BaseModel:
     def __init__(self):
         """Initializes all attributes."""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = self.created_at
-    
+        self.updated_at = self.save()
+
     def __str__(self):
         """return class name, id and attribute dictionary."""
         class_name = ""
 
-        class_name += f"{[ < class name > ] ( < self.id > ) < self.__dict__ >
-        return self.name
+        class_name += f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        return class_name
+
+    def save(self):
+        return self.created_at
+
+    def to_dict(self):
+        new_dict = {}
+        format = "%Y-%m-%dT%H:%M:%S.%f"
+        
+        for key, value in self.__dict__.items():
+            if key == "created_at" or key == "updated_at":
+                new_dict[key] = value.isoformat('-')
+            new_dict[key] = value
+
+        new_dict["__class__"] = self.__class__.__name__
+        return new_dict
 	
