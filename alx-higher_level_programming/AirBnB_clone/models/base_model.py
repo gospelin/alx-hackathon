2dc9bf792bbbc35a06b70b839680be1b232eb7ed
gospelin <gospelin.gi@gmail.com>
2022-12-02 01:01:27 +0100

    Solving Task 2

--- alx-higher_level_programming/AirBnB_clone/models/base_model.py
+++ alx-higher_level_programming/AirBnB_clone/models/base_model.py
@@ -7,15 +7,25 @@ attributes: id, created_at and updated_at
 methods: save() and to_json()
 """
 
-from datetime import datetime
+from datetime import datetime as dt
 import uuid
 
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initializes all attributes."""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.save()
+        format = "%Y-%m-%dT%H:%M:%S.%f"
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = dt.now().isoformat()
+            self.updated_at = self.save()
+
+        for key, value in kwargs.items():
+            if key == "__class__":
+                continue
+            if key == "created_at" or key == "updated_at":
+                value = dt.strptime(value, format)
+
+            setattr(self, key, value)
 
     def __str__(self):
         """return class name, id and attribute dictionary."""
@@ -25,15 +35,18 @@ class BaseModel:
         return class_name
 
     def save(self):
-        return self.created_at
+        return dt.now().isoformat()
 
     def to_dict(self):
+        """Dictionary conversion of instance attributes"""
         new_dict = {}
         format = "%Y-%m-%dT%H:%M:%S.%f"
-        
+
         for key, value in self.__dict__.items():
             if key == "created_at" or key == "updated_at":
-                new_dict[key] = value.isoformat('-')
+                new_dict[key] = dt.strptime(value, format)
+            if not value:
+                pass
             new_dict[key] = value
 
         new_dict["__class__"] = self.__class__.__name__
