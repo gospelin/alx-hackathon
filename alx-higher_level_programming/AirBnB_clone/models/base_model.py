1a2dcddc9398dcb99ffb8397eafa0ef1d6433719
gospelin <gospelin.gi@gmail.com>
2022-12-03 15:26:54 +0100

    Fix Error: Convert datetime object to string

--- alx-higher_level_programming/AirBnB_clone/models/base_model.py
+++ alx-higher_level_programming/AirBnB_clone/models/base_model.py
@@ -7,7 +7,8 @@ attributes: id, created_at and updated_at
 methods: save() and to_json()
 """
 
-from datetime import datetime as dt
+from models import storage
+from datetime import datetime
 import uuid
 
 class BaseModel:
@@ -16,39 +17,36 @@ class BaseModel:
         format = "%Y-%m-%dT%H:%M:%S.%f"
         if not kwargs:
             self.id = str(uuid.uuid4())
-            self.created_at = dt.now().isoformat()
-            self.updated_at = self.save()
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            storage.new(self)
 
         for key, value in kwargs.items():
-            if key == "__class__":
-                continue
             if key == "created_at" or key == "updated_at":
-                value = dt.strptime(value, format)
+                value = datetime.strptime(value, format)
 
-            setattr(self, key, value)
+            if key != "__class__":
+                setattr(self, key, value)
 
     def __str__(self):
         """return class name, id and attribute dictionary."""
-        class_name = ""
 
-        class_name += f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        class_name = f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
         return class_name
 
     def save(self):
-        return dt.now().isoformat()
+        self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """Dictionary conversion of instance attributes"""
         new_dict = {}
-        format = "%Y-%m-%dT%H:%M:%S.%f"
-
         for key, value in self.__dict__.items():
             if key == "created_at" or key == "updated_at":
-                new_dict[key] = dt.strptime(value, format)
-            if not value:
-                pass
-            new_dict[key] = value
-
+                new_dict[key] = value.isoformat()
+            else:
+                new_dict[key] = value
         new_dict["__class__"] = self.__class__.__name__
+
         return new_dict
 	
