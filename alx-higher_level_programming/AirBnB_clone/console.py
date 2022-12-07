fbb693e73eb4b7d2d2693f1bed41838eec131ce1
gospelin <gospelin.gi@gmail.com>
2022-12-07 02:19:21 +0100

    Instantiate the console and add the prompt feature

--- alx-higher_level_programming/AirBnB_clone/console.py
+++ alx-higher_level_programming/AirBnB_clone/console.py
@@ -6,8 +6,14 @@ import cmd
 class HBNBCommand(cmd.Cmd):
     def __init__(self):
         super().__init__()
+        cmd.Cmd.prompt = "(hbnb) "
 
-
+    def help_quit(self):
+        print("Quit command to exit the program")
+        
+    def do_quit(self, arg):
+        self.close()
+        quit()
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
