bf620adfe898812f1ac902c13394379a47574bff
gospelin <gospelin.gi@gmail.com>
2022-12-11 11:09:56 -0800

    Feature: Quit and EOF

--- alx-higher_level_programming/AirBnB_clone/console.py
+++ alx-higher_level_programming/AirBnB_clone/console.py
@@ -2,18 +2,30 @@
 """This is the console module."""
 
 import cmd
+from sys import exit
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     def __init__(self):
         super().__init__()
         cmd.Cmd.prompt = "(hbnb) "
 
-    def help_quit(self):
-        print("Quit command to exit the program")
-        
     def do_quit(self, arg):
-        self.close()
-        quit()
+        """Quit command to exit the program"""
+        exit(1)
+
+    def do_EOF(self, args):
+        """End of File"""
+        return True
+
+    def emptyline(self):
+        print
+
+    def do_create(self, args):
+        if str(args) == "BaseModel":
+            new_model = BaseModel()
+            new_model.save()
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
